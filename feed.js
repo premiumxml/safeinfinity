7b32b930e7e725d21293ff3eba74ba066dbc4699
premiumxml <65828595+premiumxml@users.noreply.github.com>
2021-03-18 08:34:27 +0700

    Update feed.js

--- feed.js
+++ feed.js
@@ -1,7 +1,1 @@
 document.write('<script src="https://www.blogger.com/feeds/1606337331133671005/posts/default?alt=json-in-script&amp;max-results=150&amp;callback=showurl"></script>');
-
-var script = document.createElement('script');
-    script.type = 'text/javascript';
-
-    script.src = 'https://www.blogger.com/feeds/1606337331133671005/posts/default?alt=json-in-script&amp;max-results=150&amp;callback=showurl';
-    document.body.appendChild(script);
