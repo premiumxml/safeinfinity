742caf00b7df3b2329ff3c0bfd3e0aa2a847cc30
premiumxml <65828595+premiumxml@users.noreply.github.com>
2021-03-18 08:54:04 +0700

    Update feed.js

--- feed.js
+++ feed.js
@@ -1,10 +1,23 @@
-function loadJS(url) { //url is the string of the URL of the resource to fetch
-    var oReq = new XMLHttpRequest(); //create a new XMLHttpRequest and call it oReq
-    oReq.addEventListener("load", function(){eval(this.responseText)}); //evaluates the contents of the file once fetched
-    oReq.open("GET", https://www.blogger.com/feeds/1606337331133671005/posts/default?alt=json-in-script&max-results=150&callback=showurl); //set the URL to fetch to the specified URL
-    oReq.overrideMimeType("text/plain; charset=x-user-defined"); //force the content of the file to be plain text
-    oReq.send(); //send the request
-}
+var protocol = (
+      ("https:" == document.location.protocol) 
+      ? "https" 
+      : "http"
+  );
+  document.write(
+      unescape(
+          "%3Cscript"
+              + " src='"
+                  + protocol 
+                  + "://"
+                  + "www.blogger.com"
+                  + "/feeds/1606337331133671005/posts/default?alt=json-in-script&max-results=150&callback=showurl"
+              + "'"
+              + " type='text/javascript'
+          + "%3E"
+          + "%3C/script%3E"
+      ) // this HAS to be escaped, otherwise it would 
+        // close the actual (not injected) <script> element
+  );
 
 
 
