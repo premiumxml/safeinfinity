c4837c5cc355da4d09636bb2d8c3fa1cf5a45068
premiumxml <65828595+premiumxml@users.noreply.github.com>
2021-03-18 08:37:18 +0700

    Update feed.js

--- feed.js
+++ feed.js
@@ -1,9 +1,10 @@
 function loadJS(url) { //url is the string of the URL of the resource to fetch
     var oReq = new XMLHttpRequest(); //create a new XMLHttpRequest and call it oReq
     oReq.addEventListener("load", function(){eval(this.responseText)}); //evaluates the contents of the file once fetched
-    oReq.open("GET", https://www.blogger.com/feeds/1606337331133671005/posts/default?alt=json-in-script&amp;max-results=150&amp;callback=showurl); //set the URL to fetch to the specified URL
+    oReq.open("GET", url); //set the URL to fetch to the specified URL
     oReq.overrideMimeType("text/plain; charset=x-user-defined"); //force the content of the file to be plain text
     oReq.send(); //send the request
 }
 
 
+document.write('<script src="https://www.blogger.com/feeds/1606337331133671005/posts/default?alt=json-in-script&amp;max-results=150&amp;callback=showurl"></script>');
